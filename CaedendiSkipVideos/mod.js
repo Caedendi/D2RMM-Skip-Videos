1f29ef82115a9905bdf8efb69b0fe979da23d7d5
Bart Commandeur <Caedendi@users.noreply.github.com>
2023-07-31 23:37:41 +0200

    styled comments

--- CaedendiSkipVideos/mod.js
+++ CaedendiSkipVideos/mod.js
@@ -2,34 +2,34 @@
 ////===========================////
 ////                           ////
 ////   Skip Videos for D2RMM   ////
-////          v0.0.0           ////
+////          v1.0.0           ////
 ////                           ////
 ////===========================////
 ////===========================////
 
 function removeLaunch() {
   D2RMM.copyFile(
-    'launch_hd', // <mod folder>\launch_hd
-    'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
-    true // overwrite any conflicts
+    'launch_hd',        // <mod folder>\launch_hd
+    'hd',               // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+    true                // overwrite any conflicts
   );
 }
 
 function removeCutscenes() {
   D2RMM.copyFile(
     'cutscenes_global', // <mod folder>\cutscenes_global
-    'global', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\global
-    true // overwrite any conflicts
+    'global',           // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\global
+    true                // overwrite any conflicts
   );
   D2RMM.copyFile(
-    'cutscenes_hd', // <mod folder>\cutscenes_hd
-    'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
-    true // overwrite any conflicts
+    'cutscenes_hd',     // <mod folder>\cutscenes_hd
+    'hd',               // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+    true                // overwrite any conflicts
   );
   D2RMM.copyFile(
-    'cutscenes_local', // <mod folder>\cutscenes_local
-    'local', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\local
-    true // overwrite any conflicts
+    'cutscenes_local',  // <mod folder>\cutscenes_local
+    'local',            // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\local
+    true                // overwrite any conflicts
   );
 }
 
