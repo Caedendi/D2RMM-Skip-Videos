a3e6864732935b457955b24d4f93dfd659802902
Bart Commandeur <Caedendi@users.noreply.github.com>
2023-07-31 20:47:44 +0200

    first version

--- CaedendiSkipVideos/mod.js
+++ CaedendiSkipVideos/mod.js
@@ -7,4 +7,39 @@
 ////===========================////
 ////===========================////
 
-// under construction
+function removeLaunch() {
+  D2RMM.copyFile(
+    'launch_hd', // <mod folder>\launch_hd
+    'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+    true // overwrite any conflicts
+  );
+}
+
+function removeCutscenes() {
+  D2RMM.copyFile(
+    'cutscenes_global', // <mod folder>\cutscenes_global
+    'global', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\global
+    true // overwrite any conflicts
+  );
+  D2RMM.copyFile(
+    'cutscenes_hd', // <mod folder>\cutscenes_hd
+    'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+    true // overwrite any conflicts
+  );
+  D2RMM.copyFile(
+    'cutscenes_local', // <mod folder>\cutscenes_local
+    'local', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\local
+    true // overwrite any conflicts
+  );
+}
+
+function applyMod() {
+  if (config.RemoveLaunch) {
+    removeLaunch();
+  }
+  if (config.RemoveCutscenes) {
+    removeCutscenes();
+  }
+}
+
+applyMod();
